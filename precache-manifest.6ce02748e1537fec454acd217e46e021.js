b2e4ad5f9deea1ca4c4117d586f5d419f4fc463a
Shree Dhushandhan <dhushandhan.sd@gmail.com>
2020-09-05 02:05:58 +0530

    Updates

---
+++ precache-manifest.6ce02748e1537fec454acd217e46e021.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b73d84bb2d43145f32d622453ecca6b3",
+    "url": "/maybelline-newyork/index.html"
+  },
+  {
+    "revision": "11d27d4866b7cedb4bc4",
+    "url": "/maybelline-newyork/static/css/main.c42a0e34.chunk.css"
+  },
+  {
+    "revision": "670b5126c385427f26e9",
+    "url": "/maybelline-newyork/static/js/2.5e61821c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/maybelline-newyork/static/js/2.5e61821c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11d27d4866b7cedb4bc4",
+    "url": "/maybelline-newyork/static/js/main.bfd52377.chunk.js"
+  },
+  {
+    "revision": "1de7601253baca882d74",
+    "url": "/maybelline-newyork/static/js/runtime-main.4ba68bd6.js"
+  },
+  {
+    "revision": "78eee9877de6b20190458d11bb212d36",
+    "url": "/maybelline-newyork/static/media/babylips.78eee987.png"
+  },
+  {
+    "revision": "2a26de687c64eb5f111cc3284c735fea",
+    "url": "/maybelline-newyork/static/media/eyeshades.2a26de68.jpeg"
+  },
+  {
+    "revision": "11dd962575123c48651f215a5f55e60d",
+    "url": "/maybelline-newyork/static/media/lips.11dd9625.jpg"
+  },
+  {
+    "revision": "9e073253fe2ea70a8a1f836161ab768c",
+    "url": "/maybelline-newyork/static/media/lips1.9e073253.png"
+  },
+  {
+    "revision": "4de3b46434bc95fd3d7737d6d0f44941",
+    "url": "/maybelline-newyork/static/media/lips2.4de3b464.png"
+  },
+  {
+    "revision": "aace58cdd0552b3f9a89b2a20e2714cd",
+    "url": "/maybelline-newyork/static/media/lips3.aace58cd.png"
+  },
+  {
+    "revision": "fe87ede4aa072202fe09acba76f76341",
+    "url": "/maybelline-newyork/static/media/lipsmodel.fe87ede4.png"
+  },
+  {
+    "revision": "0381693cef8b4347dfc28c6f1b70d0e6",
+    "url": "/maybelline-newyork/static/media/modelimage.0381693c.png"
+  },
+  {
+    "revision": "188d834dc04ae6c1ae47c03636085f59",
+    "url": "/maybelline-newyork/static/media/nail1.188d834d.png"
+  },
+  {
+    "revision": "5231ec5fec2653792eb8ca085894f778",
+    "url": "/maybelline-newyork/static/media/nail2.5231ec5f.png"
+  },
+  {
+    "revision": "9bdd6f72ecf5137322dfba08a107d4fb",
+    "url": "/maybelline-newyork/static/media/nail3.9bdd6f72.png"
+  },
+  {
+    "revision": "a0bea7be7bc5f91fc2d11253bd61a5cf",
+    "url": "/maybelline-newyork/static/media/nails.a0bea7be.jpg"
+  },
+  {
+    "revision": "12206564013bfcd2bd0090cbdaa1a0e1",
+    "url": "/maybelline-newyork/static/media/ukgrafix.12206564.png"
+  }
+]);
